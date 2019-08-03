f90eb56e24b3cef643c6caa187aaceb5c01d7d4b
Leetcode <43645876+JINGbw@users.noreply.github.com>
2019-08-03 22:01:38 +0800

    Update 33.搜索旋转排序数组.cpp

--- 数组/33.搜索旋转排序数组.cpp
+++ 数组/33.搜索旋转排序数组.cpp
@@ -4,6 +4,7 @@ public:
         //题目：在旋转前是升序的旋转数组中寻找目标值，返回目标值的索引
         //思路：直接用二分法
         //1.[mid]==target，找到了
+        //mid在有序的一侧/无序的一侧
         //2. [mid]<[r]
             // target在有序的一侧
             //target在无序的一侧
@@ -20,13 +21,19 @@ public:
             int mid = left+(right-left)/2;
             if(nums[mid]==target)
                 return mid;
+            //1. mid在有序的一侧还是无序的一侧
+            //2. 目标在mid左边还是右边
             if(nums[mid]<nums[right]){
+                //mid在有序的一侧
                 if (nums[mid]<target&&target<=nums[right])
+                    //目标在mid右边
                     left = mid+1;
                 else 
+                    //目标在mid左边
                     right = mid-1;
             }
             else {
+                //mid在无序的一边
                 if(nums[mid]>target&&target>=nums[left])
                     right = mid-1;
                 else 
